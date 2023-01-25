8a4bfeb085731fd54ae7c41b45c15f524500257e
Elgius <114131653+Elgius@users.noreply.github.com>
2023-01-25 10:33:47 +0500

    updates

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,24 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../sidebar';
 import './index.scss';
 
 const Layout = () => {
-    return <>
-    <Sidebar>
-        
-    </Sidebar>
-    </>
+    return(
+        <div className="App">
+            <Sidebar />
+            <div classroom = 'page'>
+                <span classroom = 'tags top-tags'>&lit;body&gt;</span>
+
+                <Outlet />
+
+                <span classroom = 'tags bottom-tags'>
+                    &lit;/body&gt;
+                    <br />
+                    <span classroom = 'bottom-tag-html'></span>
+                </span>
+            </div>
+        </div>
+    )
 }
 
 export default Layout
--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -2,7 +2,8 @@ import './index.scss'
 import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/Main.png'
 import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
+import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 
 const Sidebar = () => (
     <div className='nav-bar'>
@@ -22,6 +23,22 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color = '#4d4d4e' />            
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank"
+                rel='noreferrer'
+                href="https://www.linkedin.com/in/elgius-espin-ba247816b/">
+                <FontAwesomeIcon icon={faLinkedin} color = '#4d4d4e' />
+                </a>
+            </li>
+            <li>
+                <a target="_blank"
+                rel='noreferrer'
+                href="https://github.com/Elgius">
+                <FontAwesomeIcon icon={faGithub} color = '#4d4d4e' />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
